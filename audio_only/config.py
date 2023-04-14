31047c1c5c1c2938f22dfc751514cd37d9612f8a
s-ciciala <s1834237@ed.ac.uk>
2023-04-14 18:26:46 +0100

    lost audio

--- audio_only/config.py
+++ audio_only/config.py
@@ -64,7 +64,7 @@ args["LIMITGPU"] = True
 # args["GPU"] = "cuda:1,2"
 ##ELLSWORTH CONFIG
 args["GPUID"] = [0,1,2,3]
-args["BATCH_SIZE"] = 10 #minibatch size
+args["BATCH_SIZE"] = 8 #minibatch size
 ##Dudley CONFIG
 # args["GPUID"] = [0,1,2,3,4,5,]
 # args["BATCH_SIZE"] = 128 #minibatch size
